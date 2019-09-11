4e3055eb473076fdff4fbae7e657705f1ae49e56
kr8n3r <jani@kraner.dev>
2019-09-11 13:48:35 +0100

    Update with js

--- app/assets/javascripts/live_search.js
+++ app/assets/javascripts/live_search.js
@@ -17,6 +17,7 @@
     this.$loadingBlock = options.$results.find('#js-loading-message')
     this.$sortBlock = options.$results.find('#js-sort-options')
     this.$paginationBlock = options.$results.find('#js-pagination')
+    this.$suggestionsBlock = $('#js-suggestions')
     this.action = this.$form.attr('action') + '.json'
     this.$atomAutodiscoveryLink = options.$atomAutodiscoveryLink
     this.baseTitle = $("meta[name='govuk:base_title']").attr('content') || document.title
@@ -218,6 +219,15 @@
     this.updateElement(this.$sortBlock, results.sort_options_markup)
     this.bindSortElements()
   }
+  LiveSearch.prototype.updateSuggestions = function updateSuggestions (results, action) {
+    if (action !== $.param(this.state)) { return }
+    for (var i = 0; i < results.suggestions.length; i++) {
+      var suggestion = results.suggestions[i]
+      this.$suggestionsBlock.find('a')
+        .text(suggestion.keywords)
+        .attr('href', suggestion.link)
+    }
+  }
 
   LiveSearch.prototype.bindSortElements = function bindSortElements () {
     this.$orderSelect = this.$form.find('.js-order-results')
@@ -320,6 +330,7 @@
       this.updateElement(this.$facetTagBlock, results.facet_tags)
       this.updateElement(this.$countBlock, results.total)
       this.updateElement(this.$paginationBlock, results.next_and_prev_links)
+      this.updateSuggestions(results, action)
       this.updateSortOptions(results, action)
       this.$atomAutodiscoveryLink.attr('href', results.atom_url)
       this.$loadingBlock.text('').hide()
